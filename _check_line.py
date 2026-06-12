f7a36b4cc26d7bfacdca7f49b39ef52de933b7e4
Ayush Mathur <Ayush.Mathur@jda.com>
2026-06-12 23:13:10 +0530

    chore: remove all dev/debug scripts, clean workspace to production files only

--- _check_line.py
+++
@@ -1,7 +0,0 @@
-with open('backend/templates/index.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Get line 7079 (0-indexed = 7078)
-target_line_num = 7079
-for i in range(max(0, target_line_num - 5), min(len(lines), target_line_num + 5)):
-    print(f"Line {i+1}: {lines[i]}", end='')
--- _find_qmarks.py
+++
@@ -1,7 +0,0 @@
-import re
-FILE = 'backend/templates/index.html'
-lines = open(FILE, encoding='utf-8').readlines()
-
-for i, line in enumerate(lines, 1):
-    if re.search(r'>\?+<', line):
-        print(f"L{i}: {line.rstrip()[:120]}")
